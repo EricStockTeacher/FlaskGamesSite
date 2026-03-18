3b6e3f9e0a90548256d08ef4a4ed7f191769931d
Eric Stock <eric.stock@K202Domain.CSN.com>
2026-03-18 10:12:36 -0230

    Added server session handling to separate user data

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
+from flask_session import Session
 
 app = Flask(__name__)
 
-games = {}
+app.config['SESSION_PERMANENT'] = True
+app.config['SESSION_TYPE'] = "filesystem"
+
+Session(app)
+
+#app.secret_key = "mysecretkey"
+#games = {}
 
 @app.route('/')
 def hello_world():
@@ -14,14 +21,23 @@ def enter_data():
 
 @app.route('/greet', methods=['POST'])
 def greet_user():
-    name = request.form['username']
-    game = request.form['game']
-    games[name] = game
-    return render_template('greet.html', name=name, game=game)
+    game_name = request.form['game']
+    platform = request.form['platform']
+    value = request.form['value']
+    year = request.form['year']
 
-@app.route('/storage')
-def storage():
-    return render_template('storage.html', games=games)
+    if "games" in session:
+        session["games"][game_name] = {'platform':platform, 'value':value, 'year':year}
+    else:
+        session["games"] = {}
+        session["games"][game_name] = {'platform': platform, 'value': value, 'year': year}
+    #session[game_name] = {'platform':platform, 'value':value, 'year':year}
+    return render_template('greet.html', game_name=game_name)
+
+@app.route('/display')
+def display():
+    print(session)
+    return render_template('display.html', games=session.get("games", {}))
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
