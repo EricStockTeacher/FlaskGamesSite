a33c8d63e2daa2d81d76847d7f4c7739500e80b4
Eric Stock <eric.stock@K202Domain.CSN.com>
2026-03-20 10:30:54 -0230

    Setup lottery checker app

--- main.py
+++ main.py
@@ -1,43 +1,42 @@
-from flask import Flask, render_template, request, session
-from flask_session import Session
+from flask import Flask, render_template, request
+
 
 app = Flask(__name__)
 
-app.config['SESSION_PERMANENT'] = True
-app.config['SESSION_TYPE'] = "filesystem"
 
-Session(app)
-
-#app.secret_key = "mysecretkey"
-#games = {}
 
 @app.route('/')
 def hello_world():
     return render_template("main.html")
 
-@app.route('/data' )
+@app.route('/check' )
 def enter_data():
-    return render_template('game.html')
+    return render_template('check.html')
 
-@app.route('/greet', methods=['POST'])
-def greet_user():
-    game_name = request.form['game']
-    platform = request.form['platform']
-    value = request.form['value']
-    year = request.form['year']
+@app.route('/award', methods=['POST'])
+def get_award():
+    num1 = int(request.form['num1'])
+    num2 = int(request.form['num2'])
+    num3 = int(request.form['num3'])
 
-    if "games" in session:
-        session["games"][game_name] = {'platform':platform, 'value':value, 'year':year}
-    else:
-        session["games"] = {}
-        session["games"][game_name] = {'platform': platform, 'value': value, 'year': year}
-    #session[game_name] = {'platform':platform, 'value':value, 'year':year}
-    return render_template('greet.html', game_name=game_name)
+    winning_numbers = {11,22,33}
+    count = 0
+    if num1 in winning_numbers:
+        count += 1
+    if num2 in winning_numbers:
+        count += 1
+    if num3 in winning_numbers:
+        count += 1
 
-@app.route('/display')
-def display():
-    print(session)
-    return render_template('display.html', games=session.get("games", {}))
+    prize = "Try Again"
+    if count == 1:
+        prize = "Free Ticket"
+    elif count == 2:
+        prize = "Concert"
+    elif count == 3:
+        prize = "$10000"
+
+    return render_template('award.html', prize=prize)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
